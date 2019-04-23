fe5b28444694708565b1fccbc38c6310d15b38a1
ben-schulz <benjamin.john.schulz@gmail.com>
2019-04-23 13:26:32 -0500

    feat(digest): output dataframe.
    so that exploratory statistics
    can be easily computed
    on a given text.

--- teeth/digest.py
+++ teeth/digest.py
@@ -1,3 +1,6 @@
+import pandas
+import nltk.probability
+
 from teeth.tokenizer import tokenize
 from teeth.filter import no_separators
 from teeth.stemmer import stem
@@ -7,12 +10,21 @@ class Digest:
     def __init__( self, text ):
 
         tokens = tokenize( text )
-        words = no_separators( tokens )
-
         self.tokens = list( tokens )
 
+        self.words = list( no_separators( self.tokens ) )
+        self.word_freqdist = nltk.probability.FreqDist( self.words )
+
         self.raw_vocab = set(
-            map( lambda x: x.lower(), words ) )
+            map( lambda x: x.lower(), self.words ) )
 
         word_stems = stem( no_separators( tokens ) )
         self.stemmed_vocab = set( word_stems )
+
+
+    def to_dataframe( self ):
+
+        d = { k: [ k, v ] for k, v in self.word_freqdist.items() }
+
+        return pandas.DataFrame.from_dict( d, orient='index',
+                                 columns=[ 'lexeme', 'count' ] )
